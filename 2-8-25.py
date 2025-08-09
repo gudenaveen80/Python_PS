ae2816b01e4566440e3d4334c3590c1ebb407acb
gudenaveen80 <gudenaveen80@gmail.com>
2025-08-09 21:35:43 +0530

    Add files via upload

---
+++ 2-8-25.py
@@ -0,0 +1,22 @@
+#functions 
+# def simple_cal(num1,num2=10):
+#     print(num1+num2)
+# simple_cal(20)
+# simple_cal(10)
+# # simple_cal()
+
+ 
+     
+    
+# def sum(num1,num2,num3):
+#     num4=num1+num2+num3
+    
+# def sum(num1,num2,num3,num4):
+#     num5=num1+num2+num3+num4
+sum(10,20)
+sum(10,20,30)
+sum(10,20,30,40)
+     
+    
+
+
