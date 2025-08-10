966e80aee255ef5a330f727c17b1ea69063818b4
gudenaveen80 <gudenaveen80@gmail.com>
2025-08-10 22:19:04 +0530

    Add files via upload

---
+++ 10-8-25-PS.py
@@ -0,0 +1,25 @@
+
+# Hallow Square
+# n = 5
+# for i in range(n):
+#     for j in range(n):
+#         if i == 0 or i==n-1 or j == 0 or j==n-1 :
+#             print('*', end=' ')
+#         else:
+#             print(' ', end=' ')
+#     print()
+
+#  *  *  *  *  * 
+#  *           * 
+#  *           * 
+#  *  *  *  *  * 
+
+n=10
+m=3
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0 or j==n-1:
+            print("*",end='')
+        else:
+            print(' ',end='')
+    print()
